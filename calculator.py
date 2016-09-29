c763c8904d40df23773808bc82d57ac5e252fc5b
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 17:06:18 -0700

    final calculator2 with data

---
+++ calculator.py
@@ -0,0 +1,42 @@
+"""
+calculator.py
+
+Using our arithmetic.py file from Exercise02, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+
+# Your code goes here
+while True: 
+
+	user_input = raw_input('>: ')
+	tokens = user_input.split(' ')
+
+	if tokens[0] == '+':
+		print add(int(tokens[1]), int(tokens[2]))
+
+	if tokens[0] == '-':
+		print subtract(int(tokens[1]), int(tokens[2]))
+
+	if tokens[0] == '*':
+		print multiply(int(tokens[1]), int(tokens[2]))
+
+	if tokens[0] == '/':
+		print divide(int(tokens[1]), int(tokens[2]))
+
+	if tokens[0] == 'square':
+		print square(int(tokens[1]), int(tokens[2]))
+
+	if tokens[0] == 'cube':
+		print cube(int(tokens[1]))
+
+	if tokens[0] == 'pow':
+		print power(int(tokens[1]), int(tokens[2]))
+
+	if tokens[0] == '%':
+		print mod(int(tokens[1]), int(tokens[2]))
+
+	if tokens[0] == 'q':
+		break
